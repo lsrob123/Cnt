0554785065bc750b4d319324ee4ce8dd7bbbef4c
Lixin Yang <lsrob@hotmail.com>
2017-11-27 22:53:48 +1100

    C_n: Added initial bottom bar

---
+++ CntApp/CntApp/Utilities/Views/ContentGridView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CntApp.Utilities.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContentGridView : Grid
+    {
+        public ContentGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
