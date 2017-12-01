a33a8de0b53a1ad0922ec35d8e00d21ee06c3109
lixin <lsrob@hotmail.com>
2017-12-01 16:33:19 +1100

    a

--- CntApp/CntApp.Android/MainActivity.cs
+++ CntApp/CntApp.Android/MainActivity.cs
@@ -1,27 +1,23 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
-namespace CntApp.Droid
-{
-    [Activity(Label = "CntApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
+namespace CntApp.Droid {
+    [Activity(Label = "CntApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity {
+        protected override void OnCreate(Bundle bundle) {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            CachedImageRenderer.Init();
+            Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
 }
-
--- CntApp/CntApp.iOS/AppDelegate.cs
+++ CntApp/CntApp.iOS/AppDelegate.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿using FFImageLoading.Forms.Touch;
 using Foundation;
 using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
-namespace CntApp.iOS
-{
+namespace CntApp.iOS {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-    {
+    public class AppDelegate : FormsApplicationDelegate {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -20,9 +17,9 @@ namespace CntApp.iOS
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            global::Xamarin.Forms.Forms.Init();
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options) {
+            Forms.Init();
+            CachedImageRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
