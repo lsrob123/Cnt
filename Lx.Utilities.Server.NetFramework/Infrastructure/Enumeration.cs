d5bc3af407b2523dbf3e5aecf046b12a4dca2e10
Lixin Yang <lsrob@hotmail.com>
2017-12-25 21:34:29 +1100

    101 Restructured utilities

---
+++ Lx.Utilities.Server.NetFramework/Infrastructure/Enumeration.cs
@@ -0,0 +1,23 @@
+﻿using Lx.Utilities.Services.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lx.Utilities.Server.NetFramework.Infrastructure
+{
+    public abstract class Enumeration : EnumerationBase
+    {
+        protected Enumeration()
+        {
+        }
+
+        protected Enumeration(int value, string name) : base(value, name)
+        {
+        }
+
+        protected Enumeration(EnumerationBase other) : base(other)
+        {
+        }
+
+        [MaxLength(128)]
+        public override string Name { get; set; }
+    }
+}
